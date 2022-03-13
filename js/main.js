86121c8d5d1c4f54b0e50028f10fdc0bb3cbe7a8
Mohamad Salimi <mohamad.salimi@hotmail.com>
2022-03-13 20:13:27 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+let scrollBtn = document.querySelector(".scrollBtn");
 
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 500 ||
+        document.documentElement.scrollTop > 500
+    ) {
+        scrollBtn.classList.add("active");
+    } else {
+        scrollBtn.classList.remove("active");
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+scrollBtn.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+VanillaTilt.init(document.querySelector(".mockup-img"), {
+    startX: 45,
+    startY: 45,
+    reset: false,
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".mockup-img"));
